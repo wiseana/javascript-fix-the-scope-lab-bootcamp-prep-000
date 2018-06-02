ca0e664fefe0fa1c83bf81d3115ec76d360c9c4a
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-02 01:41:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,8 +19,8 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function step1() {
-  return function step2() {
+var funkyFunction = function () {
+  return function () {
     return "FUNKY!"
   }
 }
@@ -28,4 +28,4 @@ var funkyFunction = function step1() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction(); 
-step2();
+theFunk();
